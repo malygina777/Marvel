8746e3a8bb792aa7f1b7f2a6fc3f98be59e1d74d
malygina777 <159162512+malygina777@users.noreply.github.com>
2026-05-04 19:46:21 +0200

    Create marvel.js
    ajout d'un fichier

---
+++ api/marvel.js
@@ -0,0 +1,13 @@
+
+
+export default async function handler(req, res) {
+  const { path = "" } = req.query;
+
+  const response = await fetch(
+    https://marvel-server-zeta.vercel.app/${path}
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
