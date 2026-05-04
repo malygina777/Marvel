4177ba928d373ceff2f8f09c803a55424a14d389
malygina777 <159162512+malygina777@users.noreply.github.com>
2026-05-04 20:03:29 +0200

    Update marvel.js
    add ""

--- api/marvel.js
+++ api/marvel.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const { path = "" } = req.query;
 
   const response = await fetch(
-    https://marvel-server-zeta.vercel.app/${path}
+  `https://marvel-server-zeta.vercel.app/${path}`
   );
 
   const data = await response.json();
